fc0b99c35a67aa7d14331725606866c473f39ddc
Veera Sivarajan <sveera.2001@gmail.com>
2020-10-24 22:43:04 +0530

    Added: read_line() with better error message

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 void input_loop(void) {
     char *line;
     char **args;
-    int status;
-
+    int status;                 // get input and call necessary function to 
+                                // execute the command
     do {
         printf("> ");
         line = read_line();
@@ -15,6 +15,43 @@ void input_loop(void) {
         free(args);
     } while (status);
 }
+
+# define RL_BUFSIZE 1024
+
+char *read_line(void) {
+    int bufsize = RL_BUFSIZE;
+    int position = 0;
+    char *buffer = (char *) malloc(bufsize);
+    int input_char;
+
+    if (!buffer) {
+        fprintf(stderr, "read_line: buffer allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        input_char = getchar();
+
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else {
+            buffer[position] = c;
+        }
+        position += 1;
+
+        if (position >= bufsize) {
+            bufsize += RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "read_line: buffer reallocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+        
 
 int main(int argc, char **argv) {
     input_loop();
