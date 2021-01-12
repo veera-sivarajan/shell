eb8b8d55096bb6ac319ab37e16cf7d0be030756d
Veera Sivarajan <sveera.2001@gmail.com>
2021-01-12 11:27:19 +0530

    READLINE: replace GNU readline with linenoise: fix unfreed pointer

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 # include "execute.h"
 # include "builtin.h"
 # include <string.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 # include <unistd.h>
+# include "linenoise.h" 
 
 # define RESET "\x1B[0m"
 # define BLUE "\e[1;34m"
@@ -13,42 +12,48 @@
 
 void input_loop (elem **table) {
     char *line;
-    char cwd[SIZE];
-    char cmd[SIZE];
-    char reset[SIZE];
-    char **args;
-    int status;                 // get input and call necessary function to 
+    // char cwd[SIZE];
+    // char cmd[SIZE];
+    // char reset[SIZE];
+    // char **args;
+    int status = 1;                 // get input and call necessary function to 
                                 // execute the command
     do {
-        strcpy(cmd, BLUE);
-        strcpy(reset, RESET);
-        getcwd(cwd, sizeof(cwd));
-        strcat(cmd, cwd);
-        strcat(cmd, reset);
-        strcat(cmd, "$$ ");
-        line = readline(cmd);
-        add_history(line);
-        args = split_line(line);
-        for (int i = 0; args[i]; ++i) {
-            printf("ARGS: %s\n", args[i]);
+        // strcpy(cmd, BLUE);
+        // strcpy(reset, RESET);
+        // getcwd(cwd, sizeof(cwd));
+        // strcat(cmd, cwd);
+        // strcat(cmd, reset);
+        // strcat(cmd, "$$ ");
+        // line = readline(">> ");
+        // add_history(line);
+        line = linenoise(">> "); 
+        if (strcmp(line, "exit") == 0) {
+            status = 0;
         }
-        if (is_alias(line)) {
-            status = alias_handler(table, line);
-        }
-        else if (is_builtin(args)) {
-            status = builtin_handler(table, args);
-        }
-        else {
-            status = execute_command(args);
-        }
+        // args = split_line(line);
+        // if (is_alias(line)) {
+        //     status = alias_handler(table, line);
+        // }
+        // else if (is_builtin(args)) {
+        //     status = builtin_handler(table, args);
+        // }
+        // else {
+        //     status = execute_command(args);
+        // }
         free(line);
     } while (status);
+    // for (int i = 0; args[i]; ++i) {
+    //     printf("ARGS: %s\n", args[i]);
+    //     free(args[i]);
+    // }
 }
 
 int main (int argc, char **argv) {
     printf("Hello, %s\n", getenv("USER"));
     // load_table();
-    elem **table = calloc(NUM_ELE, sizeof(elem));
+    // elem **table = calloc(NUM_ELE, sizeof(elem));
+    elem **table;
     // insert_alias(table, "cl", "cd /home/veera");
     // insert_alias(table, "lock", "loginctl lock-session");
     // insert_alias(table, "books", "okular sujatha.pdf"); 
@@ -57,10 +62,10 @@ int main (int argc, char **argv) {
     // for (int i = 0; table[i]; ++i) {
     //     printf("%s\n", table[i]->alias);
     // }
-    for (int i = 0; table[i]; ++i) {
-        printf("Freeing table element...\n");
-        free(table[i]);
-    }
-    free(table);
+    // for (int i = 0; table[i]; ++i) {
+    //     printf("Freeing table element...\n");
+    //     free(table[i]);
+    // }
+    // free(table);
     return EXIT_SUCCESS;
 }
