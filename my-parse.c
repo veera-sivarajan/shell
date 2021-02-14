ef74d6e39967da0d99a10612123874cea3b8c4cd
Veera Sivarajan <sveera.2001@gmail.com>
2021-02-14 16:34:07 -0500

    Tokenize input and store in command_struct
    Piped commands will be tokenized and stored in a struct containing
    each individual command and total number of commands to be
    executed.

--- my-parse.c
+++ my-parse.c
@@ -6,17 +6,38 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-void split_pipe (char *input) {
+typedef struct command_struct {
+    char **command;
+    int num_cmds;
+} command;
+
+void split_pipe (char *input, command *cmd_table) {
+    int i = 0;
     char *split = strtok(input, "|");
-    printf("%s\n", split);
+    // printf("%s\n", split);
+    cmd_table->command[i] = (char *) malloc(100);
+    strcpy(cmd_table->command[i], split);
     while ((split = strtok(NULL, "|")) != NULL) {
-        printf("%s\n", ++split);
+        i += 1;
+        // printf("%s\n", ++split);
+        cmd_table->command[i] = (char *) malloc(100);
+        strcpy(cmd_table->command[i], ++split);
     }
+    cmd_table->num_cmds = i;
 }
 
 int main (void) {
+    command *cmd_table = (command *) calloc(1000, sizeof(command));
+    cmd_table->command = (char **) malloc(100 * sizeof(char *));
     char *input = readline(">> ");
-    split_pipe(input);
+    split_pipe(input, cmd_table);
+    // for (int i = 0; cmd_table->command[i] != NULL; ++i) {
+    for (int i = 0; i <= cmd_table->num_cmds; ++i) {
+        printf("%s\n", cmd_table->command[i]);
+        free(cmd_table->command[i]);
+    }
+    free(cmd_table->command);
+    free(cmd_table);
     free(input);
     return 0;
 }
