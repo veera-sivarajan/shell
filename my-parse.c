9d46dd3f6343ef639273553d46a7039164072056
Veera Sivarajan <sveera.2001@gmail.com>
2021-02-14 14:40:03 -0500

    Use strtok() to parse for pipes
    Previously, I was trying to parse for pipes using my own function
    but I realized strtok() does it much better than I could do. So,
    now I'm able to get all the individual commands in a command
    containing multiple pipes.

--- my-parse.c
+++ my-parse.c
@@ -3,12 +3,15 @@
 # include <stdlib.h>
 # include "parse.h" 
 
+# include <readline/readline.h>
+# include <readline/history.h>
 char **pipe_parser (char *input) {
-    char **result = (char **) malloc(1000 * sizeof(char *)); 
+    char **result = (char **) calloc(1000, sizeof(char *)); 
     int num_cmds = 0; 
     int index = 0;
     result[0] = (char *) malloc(100);
-    for (int i = 0; i < strlen(input); ++i) {
+    size_t size = strlen(input);
+    for (int i = 0; i < size; ++i) {
         if (input[i] == '|') {
             // result[num_cmds][index] = '\0';
             num_cmds += 1;
@@ -23,18 +26,30 @@ char **pipe_parser (char *input) {
             index += 1;
         }
     }
+    // for (int i = 0; result[i] != NULL; ++i) {
+    //     free(result[i]);
+    // }
+    // free(result);
     return result;
+}
+
+void split_pipe (char *input) {
+    char *split = strtok(input, "|");
+    while (split != NULL) {
+        printf("%s\n", split);
+        split = strtok(NULL, "|");
+    }
 }
 
 // void parse_wrapper (char *input) {
 //     char **cmd_list = pipe_parser(input);
 //     for (int i = 0; cmd_list[i] != NULL; ++i) {
-//         // char **result = split_line(cmd_list[i]);
-//         // printf("COMMAND\n");
-//         // for (int i = 0; result[i]; ++i) {
-//         //     printf("  String: %s\n", result[i]);
-//         // }
-//         // free(result);
+//         char **result = split_line(cmd_list[i]);
+//         printf("COMMAND\n");
+//         for (int i = 0; result[i]; ++i) {
+//             printf("  String: %s\n", result[i]);
+//         }
+//         free(result);
 //         free(cmd_list[i]);
 //     }
 //     free(cmd_list);
@@ -42,11 +57,19 @@ char **pipe_parser (char *input) {
 
 int main (void) {
     // parse_wrapper("ls | cat");
-    char **result = pipe_parser("git log --format='%an' | uniq -c");
-    for (int i = 0; result[i] != NULL; ++i) {
-        free(result[i]);
-    }
-    free(result);
+    // parse_wrapper("git log --format='%an' | uniq -c");
+    // char **result = split_line("git log --format='%an' | uniq -c");
+    // char *input = readline(">> ");
+    // char **result = pipe_parser(input);
+    // for (int i = 0; i < 2; ++i) {
+    //     printf("%s\n", result[i]);
+    //     free(result[i]);
+    // }
+    // free(input);
+    // free(result);
+    char *input = readline(">> ");
+    split_pipe(input);
+    free(input);
     return 0;
 }
         
