5278b31945499bdd322972d9880b7e86d904c826
Veera Sivarajan <sveera.2001@gmail.com>
2021-03-27 13:59:55 -0400

    Start implementing scanner
    I have decided to write my scanner for shell commands instead of
    relying on Lex and Yacc. Guess it will be fun

---
+++ scanner.c
@@ -0,0 +1,54 @@
+# include "scanner.h"
+
+# define NUM_COMMANDS 5
+
+// readline input is not new line terminated 
+
+typedef struct Command_Table {
+    char *command;
+    char **args;
+} command_table;
+
+command_table commands[NUM_COMMANDS];
+
+int is_lower_alpha (char input_char) {
+    return (input_char >= 97 && input_char <= 122);
+}
+
+typedef enum Redirect_Tokens {
+    SPACE_TOKEN,
+    PIPE_TOKEN,
+    GREAT_TOKEN,
+    GREATGREAT_TOKEN
+} redirect_tokens;
+    
+
+void scan_line (char *input) {
+    int current_pos = 0;
+    while (current_pos < strlen(input)) {
+        if (is_lower_alpha(input[current_pos])) {
+            printf("Lower alphabet: %c\n", input[current_pos]);
+        }
+        else {
+            switch (input[current_pos]) {
+            case ' ':
+                printf("Space Character: \n");
+                break;
+            case '|':
+                printf("Pipe Character: %c\n", input[current_pos]);
+                break;
+            case '>':
+                if (input[++current_pos] == '>') {
+                    printf("Great Great Character\n"); 
+                    current_pos += 1;
+                    break;
+                }
+                else {
+                    printf("Great Character: %c\n", input[current_pos]);
+                    break;
+                }
+            }
+        }
+        current_pos += 1;
+    }
+}
---
+++ scanner.h
@@ -0,0 +1,5 @@
+# include <stdio.h>
+# include <string.h>
+
+
+void scan_line (char *input);
