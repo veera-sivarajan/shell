4061e027eb80e09439bf2eac4d0077ee8dc3dd40
Veera Sivarajan <sveera.2001@gmail.com>
2021-03-27 15:16:09 -0400

    Tokenize every command
    Iterate over every command name and store it in a local variable.

--- scanner.c
+++ scanner.c
@@ -1,6 +1,8 @@
 # include "scanner.h"
+# include <stdlib.h>
 
 # define NUM_COMMANDS 5
+# define CMD_SIZE 100
 
 // readline input is not new line terminated 
 
@@ -24,18 +26,29 @@ typedef enum Redirect_Tokens {
     
 
 void scan_line (char *input) {
+    int input_length = strlen(input);
     int current_pos = 0;
     while (current_pos < strlen(input)) {
         if (is_lower_alpha(input[current_pos])) {
             printf("Lower alphabet: %c\n", input[current_pos]);
+            char *command = (char *) calloc(20 , CMD_SIZE);
+            while (input[current_pos] != ' ' && current_pos < strlen(input)) {
+                strncat(command, &input[current_pos], 1);
+                current_pos += 1;
+                // if (current_pos > input_length) return;
+            }
+            printf("First Command: %s\n", command);
+            free(command);
         }
         else {
             switch (input[current_pos]) {
             case ' ':
                 printf("Space Character: \n");
+                current_pos += 1;
                 break;
             case '|':
                 printf("Pipe Character: %c\n", input[current_pos]);
+                current_pos += 1;
                 break;
             case '>':
                 if (input[++current_pos] == '>') {
@@ -47,8 +60,9 @@ void scan_line (char *input) {
                     printf("Great Character: %c\n", input[current_pos]);
                     break;
                 }
+                current_pos += 1;
             }
         }
-        current_pos += 1;
     }
+    free(input);
 }
