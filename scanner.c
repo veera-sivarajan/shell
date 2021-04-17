7e620634942eb7ca657917d55a643b99c51aeaca
Veera Sivarajan <sveera.2001@gmail.com>
2021-04-17 10:58:51 -0400

    Improve Command_Table
    struct Command_Table now stores the num_args and num_cmds. So it
    will be easier to iterate over the entire table.

--- scanner.c
+++ scanner.c
@@ -7,35 +7,46 @@
 // readline input is not new line terminated 
 
 typedef struct Command_Table {
-    char *command;
-    char **args;
-} table;
+    int num_args;
+    int num_cmds;
+    char **cmds;
+} Command_Table ;
 
 int is_lower_alpha (char input_char) {
     return (input_char >= 97 && input_char <= 122);
 }
 
+// should free the input
 void scan_line (char *input) {
-    table **all_commands = (table **) malloc(NUM_COMMANDS * sizeof(table));
+    Command_Table **table = (Command_Table **) malloc(NUM_COMMANDS * sizeof(table));
     int input_length = strlen(input);
     int curr_pos = 0;
     int cmd_index = 0;
+    int arg_num = 0;
+    table[cmd_index] = (Command_Table *) malloc(100);
+    table[cmd_index]->cmds = (char **) malloc(100);
     
-    while (curr_pos < input_length) {
+    while (curr_pos <= input_length) {
         char curr_char = input[curr_pos];
-        if (is_lower_alpha(curr_char)) {
+        if (curr_char == '\0') {
+            cmd_index += 1;
+            table[cmd_index] = (Command_Table *) malloc(100);
+            table[cmd_index]->cmds = (char **) malloc(100);
+            break;
+        }
+        else if (is_lower_alpha(curr_char)) {
             char *command = (char *) calloc(20 , CMD_SIZE);
             while (curr_char != ' ' && curr_pos < input_length) {
                 strncat(command, &curr_char, 1);
                 curr_pos += 1;
                 curr_char = input[curr_pos];
             }
-            all_commands[cmd_index] = (table *) malloc(1 * sizeof(table));
-            all_commands[cmd_index]->command = (char *) malloc(100);
-            strcpy(all_commands[cmd_index]->command, command);
-            printf("Command: %s\n", command);
+            table[cmd_index]->cmds[arg_num] = (char *) malloc(100);
+            strcpy(table[cmd_index]->cmds[arg_num], command);
+            arg_num += 1; 
+            // printf("Command #: %i\n", cmd_index);
+            // printf("Command: %s\n", command);
             free(command);
-            free(all_commands[0]->command);
         } else if (curr_char == '-') {
             char *args = (char *) calloc(20, CMD_SIZE);
             while (curr_char != ' ' && curr_pos < input_length) {
@@ -49,11 +60,14 @@ void scan_line (char *input) {
         } else {
             switch (curr_char) {
             case ' ':
-                printf("Space Character\n");
+                // printf("Space Character\n");
                 curr_pos += 1;
                 break;
             case '|':
                 printf("Pipe Character: %c\n", curr_char);
+                cmd_index += 1;
+                table[cmd_index] = (Command_Table *) malloc(100);
+                table[cmd_index]->cmds = (char **) malloc(100);
                 curr_pos += 1;
                 break;
             case '>':
@@ -67,10 +81,15 @@ void scan_line (char *input) {
                     break;
                 }
                 curr_pos += 1;
+                break;
             }
         }
     }
+    printf("Arg num: %i\n", arg_num);
+    for (int i = 0; i <= cmd_index; ++i) {
+        free(table[i]->cmds);
+        free(table[i]);
+    }
     free(input);
-    free(all_commands[0]);
-    free(all_commands);
+    free(table);
 }
